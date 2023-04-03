b5723242bb91a745f795be3ae45e957f201b5007
Mr-peipei <wushumenmenmen2@gmail.com>
2023-04-03 23:06:25 +0900

    prisma serviceの作成

--- packages/backend/src/user/get-me/get-me.module.ts
+++ packages/backend/src/user/get-me/get-me.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "src/lib/prisma.service/prisma.service";
 import { GetMeUseCase } from "./get-me.use-case";
 
 @Module({
   controllers: [GetMeUseCase],
-  providers: [GetMeUseCase],
+  providers: [GetMeUseCase, PrismaService],
   exports: [GetMeUseCase],
 })
 export class GetMeModule {}
--- packages/backend/src/user/get-me/get-me.use-case.ts
+++ packages/backend/src/user/get-me/get-me.use-case.ts
@@ -1,13 +1,15 @@
 import { Controller, Injectable } from "@nestjs/common";
 import { GetMeResponse, GetMeRoute } from "src/generated/user";
+import { PrismaService } from "src/lib/prisma.service/prisma.service";
 
 @Controller()
 @Injectable()
 export class GetMeUseCase {
+  constructor(private prisma: PrismaService) {}
+
   @GetMeRoute()
   async execute(): Promise<GetMeResponse> {
-    return {
-      userId: "XXXX",
-    };
+    const user = await this.prisma.user.findFirst();
+    return user;
   }
 }
