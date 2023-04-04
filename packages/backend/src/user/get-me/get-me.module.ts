64a3cb2f908fa533074225b5a8aecf461510a540
Mr-peipei <wushumenmenmen2@gmail.com>
2023-04-04 21:28:35 +0900

    修正漏れ対応

--- packages/backend/src/user/get-me/get-me.module.ts
+++ packages/backend/src/user/get-me/get-me.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "src/lib/prisma.service/prisma.service";
 import { GetMeUseCase } from "./get-me.use-case";
 
 @Module({
   controllers: [GetMeUseCase],
-  providers: [GetMeUseCase, PrismaService],
+  providers: [GetMeUseCase],
   exports: [GetMeUseCase],
 })
 export class GetMeModule {}
