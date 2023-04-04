7a7d8ac1a0d0b59d3c27cf8e13892bb0b672b0da
Mr-peipei <wushumenmenmen2@gmail.com>
2023-04-04 21:27:35 +0900

    不要なServiceの削除

--- packages/backend/src/task/post-task/post-task.module.ts
+++ packages/backend/src/task/post-task/post-task.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "src/lib/prisma.service/prisma.service";
 import { PostTaskUseCase } from "./post-task.use-case";
 
 @Module({
   controllers: [PostTaskUseCase],
-  providers: [PostTaskUseCase, PrismaService],
+  providers: [PostTaskUseCase],
   exports: [PostTaskUseCase],
 })
 export class PostTaskModule {}
